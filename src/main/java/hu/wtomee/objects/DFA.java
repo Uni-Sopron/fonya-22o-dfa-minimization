0854ac416515056b816a2dc7a6b6b1d28775082d
Tamás Würth <tamas.wurth@rtspro.hu>
2023-01-27 18:29:25 +0100

    Using Jackson JSON library. Classes for DFA and Transition created, basic checks for DFA implemented. State might not be used.

---
+++ src/main/java/hu/wtomee/objects/DFA.java
@@ -0,0 +1,78 @@
+package hu.wtomee.objects;
+
+import java.util.List;
+
+public class DFA {
+
+    List<String> states;
+
+    List<String> alphabet;
+
+    List<Transition> transitions;
+
+    String initialState;
+
+    List<String> finalStates;
+
+    public DFA() {
+    }
+
+    public DFA(List<String> states, List<String> alphabet, List<Transition> transitions, String initialState, List<String> finalStates) {
+        this.states = states;
+        this.alphabet = alphabet;
+        this.transitions = transitions;
+        this.initialState = initialState;
+        this.finalStates = finalStates;
+    }
+
+    public List<String> getStates() {
+        return states;
+    }
+
+    public void setStates(List<String> states) {
+        this.states = states;
+    }
+
+    public List<String> getAlphabet() {
+        return alphabet;
+    }
+
+    public void setAlphabet(List<String> alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public List<Transition> getTransitions() {
+        return transitions;
+    }
+
+    public void setTransitions(List<Transition> transitions) {
+        this.transitions = transitions;
+    }
+
+    public String getInitialState() {
+        return initialState;
+    }
+
+    public void setInitialState(String initialState) {
+        this.initialState = initialState;
+    }
+
+    public List<String> getFinalStates() {
+        return finalStates;
+    }
+
+    public void setFinalStates(List<String> finalStates) {
+        this.finalStates = finalStates;
+    }
+
+    @Override
+    public String toString() {
+        return "DFA{" +
+                "states=" + states +
+                ", alphabet=" + alphabet +
+                ", transitions=" + transitions +
+                ", initialState='" + initialState + '\'' +
+                ", finalStates=" + finalStates +
+                '}';
+    }
+}
