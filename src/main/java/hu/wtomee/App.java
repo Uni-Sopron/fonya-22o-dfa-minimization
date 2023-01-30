8332ef41d9272344e0068f3f755fc4bacfe6dc0a
Tamás Würth <tamas.wurth@rtspro.hu>
2023-01-30 22:55:06 +0100

    DFA check added. Readme updated. TODO minimization

---
+++ src/main/java/hu/wtomee/App.java
@@ -0,0 +1,71 @@
+package hu.wtomee;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hu.wtomee.objects.DFA;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.Scanner;
+
+public class App {
+    static DFA dfa;
+
+    public static void main(String[] args) {
+
+        String dfaPath = readDfaJsonPath();
+
+        getDfaFromJsonFile(dfaPath);
+
+        checkDfa();
+
+        String inputString = readInputString();
+
+        runDfaWithInput(inputString);
+
+
+    }
+
+    private static void runDfaWithInput(String inputString) {
+        CharacterIterator it = new StringCharacterIterator(inputString);
+        dfa.resetState();
+        while(it.current() != CharacterIterator.DONE) {
+            if(dfa.input(String.valueOf(it.current())).isAccepting()) {
+                System.out.println("ACCEPTED");
+            };
+            it.next();
+        }
+    }
+
+    private static void checkDfa() {
+        dfa.checkStates().checkTransitions();
+    }
+
+    private static String readInputString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter input string");
+
+        return scanner.nextLine();
+    }
+
+    private static String readDfaJsonPath() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter absolute path of DFA.json");
+
+        return scanner.nextLine();
+    }
+
+    private static void getDfaFromJsonFile(String dfaPath) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            dfa = mapper.readValue(Paths.get(dfaPath).toFile(), DFA.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(dfa);
+    }
+
+}
--- src/main/java/hu/wtomee/objects/DFA.java
+++ src/main/java/hu/wtomee/objects/DFA.java
@@ -1,5 +1,6 @@
 package hu.wtomee.objects;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 public class DFA {
@@ -13,6 +14,8 @@ public class DFA {
     String initialState;
 
     List<String> finalStates;
+
+    String currentState;
 
     public DFA() {
     }
@@ -65,6 +68,83 @@ public class DFA {
         this.finalStates = finalStates;
     }
 
+    public String getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(String currentState) {
+        this.currentState = currentState;
+    }
+    public void checkTransitions() {
+        for(Transition t : transitions) {
+            if (!states.contains(t.nextState) || ! states.contains(t.fromState)) {
+                throw new InvalidParameterException("State doesnt exists in transition: " +  t.toString());
+            }
+            if (!alphabet.contains(t.symbol)){
+                throw new InvalidParameterException("Symbol doesnt exists in transition: " + t.toString());
+            }
+
+        }
+
+        System.out.println("OK! Transitions checked! ");
+    }
+
+    public DFA resetState() {
+        currentState = initialState;
+        return this;
+    }
+
+    public DFA input(String input) {
+        if (!alphabet.contains(input))
+            throw new InvalidParameterException("\"" + input + "\" is not valid");
+
+
+        if(transitions.stream().noneMatch(transition -> transition.getSymbol().equals(input) && transition.getFromState().equals(currentState)))
+            throw new InvalidParameterException("No transition found for input: " + input);
+
+        currentState = transitions.stream()
+                .filter(transition -> transition.getSymbol().equals(input) && transition.getFromState().equals(currentState))
+                .findFirst()
+                .get().nextState;
+
+        return this;
+    }
+
+    public boolean isAccepting() {
+        return finalStates.contains(currentState);
+    }
+
+    public DFA checkStates() {
+
+        // Check if States contains all finalStates
+        boolean containsAllFinalStates = states.containsAll(finalStates);
+        if (containsAllFinalStates){
+            System.out.println("OK! States containing all final states");
+        } else {
+            throw new InvalidParameterException("States not containing all final states");
+        }
+
+        // Check if States contains the initialState
+        boolean containsInitialState = states.contains(initialState);
+        if (containsInitialState) {
+            System.out.println("OK! States containing the initial state");
+        } else {
+            throw new InvalidParameterException("States not containing the initial state");
+        }
+
+        /*
+            Check if States contains all the Transitions from and to states.
+            Also check if Alphabet contains all Symbols from Transitions.
+         */
+        boolean containsTransitionStatesAndSymbols = transitions.stream().anyMatch(transition -> !states.containsAll(List.of(transition.getFromState(), transition.getNextState())) || !alphabet.contains(transition.getSymbol()));
+        if (!containsTransitionStatesAndSymbols) {
+            System.out.println("OK! States containing all transition states and Alphabet contains symbols");
+        } else {
+            throw new InvalidParameterException("States not containing all the transition states and Alphabet not containning symbols");
+        }
+
+        return this;
+    }
     @Override
     public String toString() {
         return "DFA{" +
